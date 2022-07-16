42882943a4dcf7d5c64360bc0212f667470905ee
ladantork <ladantorkzaban0@gmail.com>
2022-07-16 12:18:41 +0100

    9 Adding an interactive menu

--- directory2.rb
+++ directory2.rb
@@ -73,9 +73,32 @@ def print_cohort(students)
   cohort_index += 1
   end
 end
-students = input_students
+
+def interactive_menu
+  students = []  
+  loop do
+    puts "1. Input the students"
+    puts "2. Show the students"
+    puts "9. Exit"
+    selection = gets.chomp
+  case selection
+    when "1"
+      students = input_students
+    when "2"
+      print_header
+      print(students)
+      print_footer(students)
+    when "9"
+      exit
+    else
+      puts "I dont know what you meant,try again"  
+    end
+  end
+end
+interactive_menu
+# students = input_students
 # nothing happens untill we call the method
-print_header
-print(students)
-print_footer(students)
-print_cohort(students)
+# print_header
+# print(students)
+# print_footer(students)
+# print_cohort(students)
